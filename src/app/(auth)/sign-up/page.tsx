2ea233f03d099910dfa9c106ad26f49173dc4934
Rahul Ch <xavi3441@gmail.com>
2025-05-30 23:33:14 +0530

    04:authentication-ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import { SignUpView } from "@/modules/ui/views/sign-up-view";
+
+const Page=()=>{
+    
+     console.log("Sign Up Page");
+    return(
+       
+        <div>
+            <SignUpView/>
+        </div>
+    )
+}
+
+export default Page;
