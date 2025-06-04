a12edc7a05c92c249536d6ce306ba26abc61a629
Rahul Ch <xavi3441@gmail.com>
2025-06-04 23:30:55 +0530

    07: (b) Updated dashboard-navbar

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -24,12 +24,12 @@ export const DashboardNavbar=()=>{
     return(
         <>
         <DashboardCommand open={commandOpen} setOpen={setCommandOpen}/>
-        <nav className="flex px-4 gap-x-2 items=center py-3 border-b bg-background">
+        <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
             <Button className="size-9" variant="outline" onClick={toggleSidebar}>
                {(state==="collapsed"||isMobile) ? <PanelLeftIcon/> : <PanelLeftCloseIcon/>}
             </Button>
             <Button
-            className="h-9 w-[240px] justify-start font-formal text-muted-forground
+            className="h-9 w-[240px] justify-start font-normal text-muted-forground
             hover:text-muted-foreground"
             variant="outline"
             size="sm"
@@ -38,7 +38,7 @@ export const DashboardNavbar=()=>{
                 <SearchIcon/>
                 Search
                 <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1
-                    rounded border bg-muted px-1.5 fond-mono text-[10px] font-medium text-muted-foreground 
+                    rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground 
                 ">
                     <span className="text-xs">&#8984;</span>
                 </kbd>               
